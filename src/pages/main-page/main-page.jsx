35b217ddc6740eb4c240bdf8cee40dc50ad0242c
PaulLyu <polina.lyubeznova@geogracom.com>
2025-06-05 16:03:16 +0300

    feat(pages/main-page): добавила компонент основной страницы

---
+++ src/pages/main-page/main-page.jsx
@@ -0,0 +1,16 @@
+import { AppHeader, BurgerIngredients } from "../../components";
+import styles from "./main-page.module.css";
+import { data } from "../../data/data";
+
+export const MainPage = () => {
+  return (
+    <>
+      {" "}
+      <AppHeader />
+      <main className={styles.main}>
+        <BurgerIngredients data={data} />
+        {/* <BurgerConstructor data={data} /> */}
+      </main>
+    </>
+  );
+};
