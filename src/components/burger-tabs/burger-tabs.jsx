68bc5c74d3ee09779030985c746a6e7893cf119c
PaulLyu <polina.lyubeznova@geogracom.com>
2025-06-05 16:33:24 +0300

    feat(components/burger-tabs): добавила компонент табов

---
+++ src/components/burger-tabs/burger-tabs.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import { categories, BUN } from "../../data/categories";
+import styles from "./burger-tabs.module.css";
+
+export const BurgerTabs = ({ сhangeTab }) => {
+  const [activeTab, setActiveTab] = React.useState(BUN);
+
+  const handleChangeTab = (type) => {
+    setActiveTab(type);
+    сhangeTab(type);
+  };
+
+  return (
+    <div className={`${styles.tabs} mb-8`}>
+      {categories.map(({ key, label }) => (
+        <Tab
+          key={key}
+          value={key}
+          active={activeTab === key}
+          onClick={handleChangeTab}
+        >
+          {label}
+        </Tab>
+      ))}
+    </div>
+  );
+};
+
+BurgerTabs.propTypes = {
+  сhangeTab: PropTypes.func.isRequired,
+};
---
+++ src/components/burger-tabs/index.js
@@ -0,0 +1,1 @@
+export { BurgerTabs } from "./burger-tabs";
