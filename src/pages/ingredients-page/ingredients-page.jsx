80e67cd13914654e359e2d1204b8989072b108cb
PaulLyu <polina.lyubeznova@geogracom.com>
2025-07-18 02:48:51 +0300

    feat(pages/ingredients-page): добавила подгрузку данных ингредиента при переходе по url

--- src/pages/ingredients-page/ingredients-page.jsx
+++ src/pages/ingredients-page/ingredients-page.jsx
@@ -1,10 +1,47 @@
+import { useEffect } from "react";
+import { useParams } from "react-router";
 import { IngredientDetails } from "../../components/ingredient-details";
-import { useAppSelector } from "../../hooks";
+import { useAppSelector, useAppDispatch } from "../../hooks";
+import { fetchBurgerIngredients } from "../../services/actions";
+import { setCurrentIngredient } from "../../services/reducers/ingredient-details.reducer";
 
 export const IngredientsPage = () => {
+  const dispatch = useAppDispatch();
+  const { id } = useParams();
   const { currentIngredient } = useAppSelector(
     (state) => state.ingredientDetails
   );
+  const { data: ingredients } = useAppSelector(
+    (state) => state.burgerIngredients
+  );
+
+  useEffect(() => {
+    const fetchAndSetIngredient = async () => {
+      if (currentIngredient || !id) return;
+
+      const findAndSet = (list) => {
+        const found = list.find((item) => item._id === id);
+        if (found) {
+          dispatch(setCurrentIngredient(found));
+        }
+      };
+
+      try {
+        if (!ingredients || ingredients.length === 0) {
+          const fetchedIngredients = await dispatch(
+            fetchBurgerIngredients()
+          ).unwrap();
+          findAndSet(fetchedIngredients);
+        } else {
+          findAndSet(ingredients);
+        }
+      } catch (error) {
+        console.error("Ошибка при загрузке ингредиентов:", error);
+      }
+    };
+
+    fetchAndSetIngredient();
+  }, [dispatch, id, currentIngredient, ingredients]);
 
   return (
     <div className="text-align-center">
