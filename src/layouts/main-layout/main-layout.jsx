1afcc389667491b1b2695f7d65f5122dd009fd25
lyubeznova.polina <lyubeznova.polina@geogracom.com>
2025-08-20 15:41:58 +0300

    feat(pages/not-found-page): добавила страницу

--- src/layouts/main-layout/main-layout.jsx
+++ src/layouts/main-layout/main-layout.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router";
-import styles from './main-layout.module.css'
+import styles from "./main-layout.module.css";
 import { AppHeader } from "../../components";
 
 export const MainLayout = () => {
-    return (     
-      <>
-        <AppHeader />
-        <main className={styles.container}>
-          <Outlet />
-        </main>
-      </> 
+  return (
+    <>
+      <AppHeader />
+      <main className={styles.container}>
+        <Outlet />
+      </main>
+    </>
   );
-}
+};
