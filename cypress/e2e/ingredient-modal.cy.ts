2de68c0e3ef70645afedeb60db410b385afaff53
PaulLyu <polina.lyubeznova@geogracom.com>
2025-09-02 16:22:05 +0300

    refactor(cypress/e2e/ingredient-modal): отрефакторила тесты для модального окна

--- cypress/e2e/ingredient-modal.cy.ts
+++ cypress/e2e/ingredient-modal.cy.ts
@@ -1,51 +1,65 @@
-describe("ingredient-modal", () => {
+const SELECTORS = {
+  INGREDIENT_CARD: '[data-testid="ingredient-card"]',
+  MODAL: '[data-testid="modal"]',
+  MODAL_TITLE: '[data-testid="modal-title"]',
+  MODAL_CLOSE: '[data-testid="modal-close-button"]',
+  CARD_NAME: '[data-testid="card-ingredient-name"]',
+  MODAL_NAME: '[data-testid="figcaption-ingredient-name"]',
+};
+
+describe("Модальное окно ингредиента", () => {
   beforeEach(() => {
     cy.visit("/");
+    cy.get(SELECTORS.INGREDIENT_CARD).first().as("firstIngredient");
   });
 
-  it("должно открываться модальное окно при нажатии на карточку ингредиента", () => {
-    // Ждем загрузки ингредиентов
-    cy.get('[data-testid="ingredient-card"]').should("exist");
+  const opneModal = () => {
+    cy.get("@firstIngredient").click();
+    cy.get(SELECTORS.MODAL).should("be.visible");
+  };
 
-    // Кликаем на первый ингредиент
-    cy.get('[data-testid="ingredient-card"]').first().click();
+  const closeModal = () => {
+    cy.get(SELECTORS.MODAL_CLOSE).click();
+    cy.get(SELECTORS.MODAL).should("not.exist");
+  };
 
-    // Проверяем, что модальное окно открылось
-    cy.get('[data-testid="modal"]').should("exist");
+  describe("Взаимодействие с модальным окном", () => {
+    it("должно открывать модальное окно при клике на карточку ингредиента", () => {
+      opneModal();
+      cy.get(SELECTORS.MODAL_TITLE).should("contain", "Детали ингредиента");
+    });
 
-    // Проверяем, что модальное окно открылось с правильнм заголовком
-    cy.get('[data-testid="modal-title"]')
-      .should("exist")
-      .contains("Детали ингредиента");
+    it("должно закрывать модальное окно при клике на кнопку закрытия", () => {
+      opneModal();
+      closeModal();
+    });
+
+    it("должно закрывать модальное окно при клике на overlay", () => {
+      opneModal();
+      cy.get(SELECTORS.MODAL).click("topLeft");
+      cy.get(SELECTORS.MODAL).should("not.exist");
+    });
   });
 
-  it("должно закрываться модальное окно при нажатии кнопки закрытия", () => {
-    // Открываем модальное окно
-    cy.get('[data-testid="ingredient-card"]').first().click();
-    cy.get('[data-testid="modal"]').should("exist");
+  describe("Содержимое модального окна", () => {
+    it("должно отображать правильное название ингредиента", () => {
+      cy.get("@firstIngredient")
+        .find(SELECTORS.CARD_NAME)
+        .invoke("text")
+        .then((ожидаемоеНазвание) => {
+          opneModal();
+          cy.get(SELECTORS.MODAL_NAME).should("have.text", ожидаемоеНазвание);
+        });
+    });
 
-    // Закрываем модальное окно
-    cy.get('[data-testid="modal-close-button"]').click();
+    it("должно отображать изображение ингредиента", () => {
+      opneModal();
+      cy.get(`${SELECTORS.MODAL} img`).should("be.visible");
+    });
 
-    // Проверяем, что модальное окно закрылось
-    cy.get('[data-testid="modal"]').should("not.exist");
-  });
-
-  it("должны отображаться правильные данные об ингредиентах в модальном окне", () => {
-    // Получаем данные первого ингредиента
-    cy.get('[data-testid="ingredient-card"]')
-      .first()
-      .find('[data-testid="card-ingredient-name"]')
-      .invoke("text")
-      .then((ingredientName) => {
-        // Кликаем на ингредиент
-        cy.get('[data-testid="ingredient-card"]').first().click();
-
-        // Проверяем модальное окно
-        cy.get('[data-testid="modal"]').should("exist");
-        cy.get('[data-testid="figcaption-ingredient-name"]')
-          .should("exist")
-          .should("contain", ingredientName);
-      });
+    it("должно отображать пищевую ценность ингредиента", () => {
+      opneModal();
+      cy.get('[data-testid="nutrition"]').should("exist");
+    });
   });
 });
